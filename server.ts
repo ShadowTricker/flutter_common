df8abfc454651fd7cf21d29686f0757eb016c8ac
Wang <zhiyu.c.wang@accenture.com>
2020-10-27 19:38:55 +0800

    [modify] server.ts

--- server.ts
+++ server.ts
@@ -7,11 +7,24 @@ const options = { hostname: '10.237.188.41', port: 3400 };
 const app = new Application();
 const router = new Router();
 
-router.get('/articles', (ctx) => {
-  ctx.response.body = {
-    name: 'test'
-  };
-});
+router
+  .get('/articles', async (ctx) => {
+    const decoder = new TextDecoder('utf-8');
+    const data = await Deno.readFile('./assets/data/articles.json');
+    ctx.response.body = {
+      articles: decoder.decode(data)
+    };
+  })
+  .get('/articles/:id', async (ctx) => {
+    const { id } = ctx.params;
+    const decoder = new TextDecoder('utf-8');
+    const dataArray = await Deno.readFile('./assets/data/articles.json');
+    const data = JSON.parse(decoder.decode(dataArray));
+    const article = data.find((item: any) => item.id === id);
+    ctx.response.body = {
+      articles: article
+    };
+  });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
